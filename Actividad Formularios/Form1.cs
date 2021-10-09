0708b93a874abde828345ded3869ee9f19d8599e
NahueBericiartua <nahue.br1@gmail.com>
2021-10-09 18:14:40 -0300

    se agrega el proyecto

---
+++ Actividad Formularios/Form1.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Nahue
+ * Date: 8/10/2021
+ * Time: 18:50
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Actividad_Formularios
+{
+	/// <summary>
+	/// Description of Form1.
+	/// </summary>
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
---
+++ Actividad Formularios/MainForm.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Nahue
+ * Date: 8/10/2021
+ * Time: 18:35
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Actividad_Formularios
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public Form formulario;
+		public MainForm()
+		{
+			formulario = new Form1();
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Btn_mostrarClick(object sender, EventArgs e)
+		{
+			formulario.Show();
+		}
+		void Btn_ocultarClick(object sender, EventArgs e)
+		{
+			formulario.Hide();
+		}
+	}
+}
